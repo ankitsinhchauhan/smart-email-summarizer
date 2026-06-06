cb6a5669418a9b9d8bdbffedb41efcd87b44b9e3
anktisinhchauhan <ankitchauhan3020@gmail.com>
2026-06-06 11:09:04 +0530

    Build smart email summarizer project

---
+++ src/summarizer.py
@@ -0,0 +1,29 @@
+from transformers import pipeline
+from src.preprocess import clean_text
+
+MODEL_NAME = "facebook/bart-large-cnn"
+
+summarizer_pipeline = pipeline(
+    "summarization",
+    model=MODEL_NAME
+)
+
+def summarize_text(
+    text: str,
+    max_length: int = 130,
+    min_length: int = 30
+) -> str:
+    cleaned_text = clean_text(text)
+
+    if len(cleaned_text) < 100:
+        return "Text is too short to summarize."
+
+    result = summarizer_pipeline(
+    cleaned_text,
+    max_length=max_length,
+    min_length=min_length,
+    do_sample=False,
+    truncation=True
+    )
+
+    return result[0]["summary_text"]
